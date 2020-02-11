9acbfc4617ae81ae66cd5b7b9eb62abc69a42abe
Isabelle HENNES <isabelle.community@gmail.com>
2020-02-11 16:20:54 +0100

    changement de variables

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ require_once 'vendor/autoload.php';
 $router = new Router($_GET['url']);
 
 //liste de nos routes
-$router->get('/acteur', "Artist.listActeur");
 $router->get('/acteur/:id', "Artist.showActeur");
+$router->get('/acteur', "Artist.listActeur");
 $router->get('/film/:id', "Film.show");
-$router->get('/', 'Home.index');
-$router->get('/realisateur', "Artist.listRealisateur");
 $router->get('/realisateur/:id',"Artist.showReal");
+$router->get('/realisateur', "Artist.listRealisateur");
+$router->get('/', 'Home.index');
+
 
 $router->run();
