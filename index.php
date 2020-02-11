912f4ca12d3308123359c2b00506f42dcc649f6d
Isabelle HENNES <isabelle.community@gmail.com>
2020-02-11 16:17:53 +0100

    ajout de routes

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ $router = new Router($_GET['url']);
 
 //liste de nos routes
 $router->get('/acteur', "Artist.listActeur");
+$router->get('/acteur/:id', "Artist.showActeur");
 $router->get('/film/:id', "Film.show");
 $router->get('/', 'Home.index');
 $router->get('/realisateur', "Artist.listRealisateur");
+$router->get('/realisateur/:id',"Artist.showReal");
 
 $router->run();
